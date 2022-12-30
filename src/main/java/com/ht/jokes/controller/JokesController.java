78a8444310a10fc4c18b78506c8ff3f564b39613
ht5929xp <ht.5929xp@gmail.com>
2022-12-29 19:07:10 -0500

    Improved implementation

--- src/main/java/com/ht/jokes/controller/JokesController.java
+++ src/main/java/com/ht/jokes/controller/JokesController.java
@@ -2,6 +2,7 @@ package com.ht.jokes.controller;
 
 import com.ht.jokes.model.Joke;
 import com.ht.jokes.services.JokesService;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JokesController {
 
-    private JokesService jkService;
+    private final JokesService jkService;
 
     public JokesController(JokesService jkService) {
         this.jkService = jkService;
--- src/main/java/com/ht/jokes/services/JokesServiceImpl.java
+++ src/main/java/com/ht/jokes/services/JokesServiceImpl.java
@@ -6,8 +6,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokesServiceImpl implements JokesService {
+
+    private final ChuckNorrisQuotes quotesService;
+
+    public JokesServiceImpl() {
+        this.quotesService = new ChuckNorrisQuotes();
+    }
+
     @Override
     public Joke getRandomJoke() {
-        return new Joke(new ChuckNorrisQuotes().getRandomQuote());
+        return new Joke(quotesService.getRandomQuote());
     }
 }
